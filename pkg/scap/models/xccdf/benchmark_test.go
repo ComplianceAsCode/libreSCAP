c91388de160cb09cfe68c84c39fc58753e2d36ff
Šimon Lukašík <isimluk@fedoraproject.org>
2020-05-15 22:37:26 +0200

    Kick-off project structure, first model, first test, CI set-up

---
+++ pkg/scap/models/xccdf/benchmark_test.go
@@ -0,0 +1,15 @@
+package xccdf_test
+
+import (
+	"testing"
+
+	"github.com/complianceascode/librescap/pkg/scap/scap_document"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRFCFeedParsing(t *testing.T) {
+	doc, err := scap_document.ReadDocumentFromFile("../../../../examples/scap/xccdf/1.2/test_xccdf_complex_check_nand.xccdf.xml")
+	assert.Nil(t, err)
+	assert.NotNil(t, doc.Benchmark)
+	assert.Equal(t, doc.Benchmark.XMLName.Space, "http://checklists.nist.gov/xccdf/1.2")
+}
